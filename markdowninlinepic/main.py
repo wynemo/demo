d4e880b43b94c82123dab6910c25fa33535e81c3
Your Name <you@example.com>
2023-04-07 14:15:17 +0000

    use PIL to determine file type

--- markdowninlinepic/main.py
+++ markdowninlinepic/main.py
@@ -1,24 +1,39 @@
 import argparse
+import io
 import os
 import re
 import uuid
 
 import requests
+from PIL import Image
+
+
+# This function downloads an image from a given URL and saves it to a local directory
+# It takes a URL as input
+# It returns the path to the downloaded image file
 
 def download_img(url):
-    # generate a unique name for the image file
-    img_name = str(uuid.uuid4()) + os.path.splitext(url)[-1]
-    # create a directory to store the images if it doesn't exist
+    # Send a GET request to the URL and store the response
+    response = requests.get(url)
+    # Print the response status code for debugging purposes
+    print(response)
+    # Determine the file extension of the image from the URL
+    file_ext = os.path.splitext(url)[-1]
+    # If the file extension cannot be determined from the URL, open the image using PIL and get the format
+    if not file_ext:
+        img = Image.open(io.BytesIO(response.content))
+        file_ext = '.' + img.format.lower()
+    # Generate a unique name for the image file using UUID
+    img_name = str(uuid.uuid4()) + file_ext
+    # Create a directory to store the images if it doesn't exist
     img_dir = 'images'
     os.makedirs(img_dir, exist_ok=True)
-    # create the full path to the image file
+    # Create the full path to the image file
     img_path = os.path.join(img_dir, img_name)
-    
-    # download the image from the url and save it to the image file
+    # Write the image content to the file
     with open(img_path, 'wb') as f:
-        r = requests.get(url)
-        print(r)
-        f.write(r.content)
+        f.write(response.content)
+    # Return the path to the downloaded image file
     return img_path
 
 def replace_img(markdown_path):
